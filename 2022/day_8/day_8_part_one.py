b26a477f8ae4fcf98834091ca3856b3fafdc8647
Tomasz Szyborski <tomasz.szyborski@hpe.com>
2022-12-08 21:24:41 +0100

    day 8 2022

---
+++ 2022/day_8/day_8_part_one.py
@@ -0,0 +1,103 @@
+"""
+--- Day 8: Treetop Tree House ---
+The expedition comes across a peculiar patch of tall trees all planted carefully in a grid.
+The Elves explain that a previous expedition planted these trees as a reforestation effort.
+Now, they're curious if this would be a good location for a tree house.
+
+First, determine whether there is enough tree cover here to keep a tree house hidden.
+To do this, you need to count the number of trees that are visible
+from outside the grid when looking directly along a row or column.
+
+The Elves have already launched a quadcopter to generate a map
+with the height of each tree (your puzzle input). For example:
+
+30373
+25512
+65332
+33549
+35390
+Each tree is represented as a single digit whose value is its height,
+where 0 is the shortest and 9 is the tallest.
+
+A tree is visible if all of the other trees between it and an edge of the grid are shorter than it.
+Only consider trees in the same row or column;
+that is, only look up, down, left, or right from any given tree.
+
+All of the trees around the edge of the grid are visible -
+since they are already on the edge, there are no trees to block the view.
+In this example, that only leaves the interior nine trees to consider:
+
+The top-left 5 is visible from the left and top.
+(It isn't visible from the right or bottom since other trees of height 5 are in the way.)
+The top-middle 5 is visible from the top and right.
+The top-right 1 is not visible from any direction; for it to be visible,
+ there would need to only be trees of height 0 between it and an edge.
+The left-middle 5 is visible, but only from the right.
+The center 3 is not visible from any direction; for it to be visible,
+there would need to be only trees of at most height 2 between it and an edge.
+The right-middle 3 is visible from the right.
+In the bottom row, the middle 5 is visible, but the 3 and 4 are not.
+With 16 trees visible on the edge and another 5 visible in the interior,
+a total of 21 trees are visible in this arrangement.
+
+Consider your map; how many trees are visible from outside the grid?
+"""
+from dataclasses import dataclass
+
+
+def read_data_file(input_file_path: str) -> list[str]:
+    with open(input_file_path) as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def process_raw_data_to_grid(forest: list[str]):
+    return [[int(tree) for tree in line] for line in forest]
+
+
+@dataclass
+class Grid:
+    data: list[list[int]]
+
+    def get_column_from_grid(self, column_number: int) -> list[int]:
+        return [row[column_number] for row in self.data]
+
+    def get_row_from_grid(self, row_number: int) -> list[int]:
+        return self.data[row_number]
+
+
+def is_tree_visible(tree_under_analysis: int, line_of_trees: list[int]) -> bool:
+    for tree in line_of_trees:
+        if tree >= tree_under_analysis:
+            return False
+    return True
+
+
+def count_visible_trees_in_the_forest(forest: Grid):
+    visible_trees = 0
+    for row_index, tree_row in enumerate(forest.data):
+        for tree_index, tree in enumerate(tree_row):
+            if row_index in [0, len(forest.data)] or tree_index in [0, len(forest.data[0])]:
+                visible_trees += 1
+                continue
+            row_of_trees = forest.get_row_from_grid(row_index)
+            column_of_trees = forest.get_column_from_grid(tree_index)
+            from_left = is_tree_visible(tree_under_analysis=tree,
+                                        line_of_trees=row_of_trees[:tree_index])
+            from_right = is_tree_visible(tree_under_analysis=tree,
+                                         line_of_trees=row_of_trees[tree_index + 1:])
+            from_top = is_tree_visible(tree_under_analysis=tree,
+                                       line_of_trees=column_of_trees[:row_index])
+            from_bottom = is_tree_visible(tree_under_analysis=tree,
+                                          line_of_trees=column_of_trees[row_index + 1:])
+            if any([from_left, from_right, from_top, from_bottom]):
+                visible_trees += 1
+    return visible_trees
+
+
+if __name__ == '__main__':
+    raw_data = read_data_file('puzzle_input.txt')
+    grid_of_trees = process_raw_data_to_grid(raw_data)
+    forest = Grid(grid_of_trees)
+    visible_trees = count_visible_trees_in_the_forest(forest)
+    print(visible_trees)
---
+++ 2022/day_8/test_day_8_part_two.py
@@ -0,0 +1,106 @@
+"""
+--- Part Two ---
+Content with the amount of tree cover available,
+the Elves just need to know the best spot to build their tree house:
+they would like to be able to see a lot of trees.
+
+To measure the viewing distance from a given tree, look up, down, left, and right from that tree;
+stop if you reach an edge or at the first tree that is the same height or taller than the tree under consideration.
+(If a tree is right on the edge, at least one of its viewing distances will be zero.)
+
+The Elves don't care about distant trees taller than those found by the rules above;
+the proposed tree house has large eaves to keep it dry,
+so they wouldn't be able to see higher than the tree house anyway.
+
+In the example above, consider the middle 5 in the second row:
+
+30373
+25512
+65332
+33549
+35390
+Looking up, its view is not blocked;
+it can see 1 tree (of height 3).
+Looking left, its view is blocked immediately;
+it can see only 1 tree (of height 5, right next to it).
+Looking right, its view is not blocked;
+it can see 2 trees.
+Looking down, its view is blocked eventually;
+it can see 2 trees (one of height 3, then the tree of height 5 that blocks its view).
+A tree's scenic score is found by multiplying together its viewing distance in each of the four directions.
+For this tree, this is 4 (found by multiplying 1 * 1 * 2 * 2).
+
+However, you can do even better: consider the tree of height 5 in the middle of the fourth row:
+
+30373
+25512
+65332
+33549
+35390
+Looking up, its view is blocked at 2 trees (by another tree with a height of 5).
+Looking left, its view is not blocked; it can see 2 trees.
+Looking down, its view is also not blocked; it can see 1 tree.
+Looking right, its view is blocked at 2 trees (by a massive tree of height 9).
+This tree's scenic score is 8 (2 * 2 * 1 * 2); this is the ideal spot for the tree house.
+
+Consider each tree on your map. What is the highest scenic score possible for any tree?
+"""
+from day_8_part_one import read_data_file, process_raw_data_to_grid, Grid
+
+def look_around(function):
+    def wrapper(*args, **kwargs):
+        print("looking started".center(80, "="))
+        result = function(*args, **kwargs)
+        print("looking finished".center(80, "="))
+        return result
+    return wrapper
+
+
+@look_around
+def number_of_trees_seen(tree_under_analysis: int, line_of_trees, **kwargs) -> int:
+    print(f"{kwargs}")
+    print(f"{line_of_trees=}")
+    for count, tree in enumerate(line_of_trees, start=1):
+        if tree >= tree_under_analysis:
+            i_see_trees_of = count
+            break
+    else:
+        i_see_trees_of = len(line_of_trees)
+    print(f"{i_see_trees_of=}")
+    return i_see_trees_of
+
+
+def test_tree_with_brest_view():
+    raw_data = read_data_file('test_input.txt')
+    grid_of_trees = process_raw_data_to_grid(raw_data)
+    forest = Grid(grid_of_trees)
+    print()
+    for row in forest.data:
+        print(row)
+    total_tree_view = 0
+    for row_index, tree_row in enumerate(forest.data):
+        for tree_index, tree in enumerate(tree_row):
+            print("INVESTIGATING TREE INITIATED".center(80,"#"))
+            print(f"investigating {tree} at [{row_index}][{tree_index}]")
+            row_of_trees = forest.get_row_from_grid(row_index)
+            column_of_trees = forest.get_column_from_grid(tree_index)
+            left = number_of_trees_seen(tree_under_analysis=tree,
+                                        line_of_trees=row_of_trees[:tree_index][::-1],
+                                        look="left")
+            right = number_of_trees_seen(tree_under_analysis=tree,
+                                         line_of_trees=row_of_trees[tree_index + 1:],
+                                         look="right")
+            up = number_of_trees_seen(tree_under_analysis=tree,
+                                      line_of_trees=column_of_trees[:row_index][::-1],
+                                      look="up")
+            down = number_of_trees_seen(tree_under_analysis=tree,
+                                        line_of_trees=column_of_trees[row_index + 1:],
+                                        look="down")
+            view = right * left * up * down
+            print(f"{view=}")
+            if view > total_tree_view:
+                total_tree_view = view
+            print("INVESTIGATING TREE COMPLETED".center(80,"#"))
+    print()
+    print(f"{total_tree_view=}")
+    assert total_tree_view == 8
