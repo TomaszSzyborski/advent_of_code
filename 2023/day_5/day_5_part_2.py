621c0fd75372a8bc415b99e482962342941203d8
TomaszSzyborski <tomasz.szyborski@gmail.com>
2023-12-09 08:29:53 +0100

    2023 day 5 - working fastest approach

--- 2023/day_5/day_5_part_2.py
+++ 2023/day_5/day_5_part_2.py
@@ -20,8 +20,8 @@ import os
 import sys
 from itertools import takewhile
 
-# mode = 'test'
-mode = 'production'
+mode = 'test'
+# mode = 'production'
 if mode == 'test':
     logging.basicConfig(encoding='utf-8', level=os.getenv("loglevel", logging.DEBUG))
 else:
@@ -43,78 +43,111 @@ if mode == 'test':
     logging.info(f"{seeds=}")
     assert len(seeds) == 27, f"{len(seeds)=} {seeds}"
 
-cleaned_data = {}
+# cleaned_data = {}
+#
+#
+# @dataclasses.dataclass
+# class Mapping:
+#     destination_range_start: int
+#     source_range_start: int
+#     range_length: int
+#
+#     def __post_init__(self):
+#         self.source_range_start = int(self.source_range_start)
+#         self.destination_range_start = int(self.destination_range_start)
+#         self.range_length = int(self.range_length)
+#
+#     def check_mapping(self, point):
+#         return self.source_range_start + self.range_length >= point >= self.source_range_start
+#
+#     def map_source_to_destination(self, point: int):
+#         return self.destination_range_start + (point - self.source_range_start)
+#
+#
+# for element in [
+#     "seed-to-soil",
+#     "soil-to-fertilizer",
+#     "fertilizer-to-water",
+#     "water-to-light",
+#     "light-to-temperature",
+#     "temperature-to-humidity",
+#     "humidity-to-location",
+# ]:
+#     index = data.index(f"{element} map:")
+#     lines = takewhile(lambda line: line != '', data[index + 1:])
+#     cleaned_data[element] = [Mapping(*line.split()) for line in lines]
+#     # logging.debug(cleaned_data[element])
+#
+# logging.info(cleaned_data)
+# logging.info(f"{len(cleaned_data)=}")
+#
+# locations = []
+# min_destination = sys.maxsize
+# for seed_range in seeds_pairs:
+#     places = copy.deepcopy(seed_range)
+#     logging.info(f"loop for {places}")
+#     for seed in seed_range:
+#         logging.debug(f"checking {seed=}")
+#         for place in places:
+#             source = place
+#             for element in [
+#                 "seed-to-soil",
+#                 "soil-to-fertilizer",
+#                 "fertilizer-to-water",
+#                 "water-to-light",
+#                 "light-to-temperature",
+#                 "temperature-to-humidity",
+#                 "humidity-to-location",
+#             ]:
+#                 destination = None
+#                 for mapping in cleaned_data[element]:
+#                     logging.debug(
+#                         f"{element=} source={source}; is_mapped={mapping.check_mapping(place)}; destination={mapping.map_source_to_destination(place)} ")
+#                     if mapping.check_mapping(place):
+#                         destination = mapping.map_source_to_destination(place)
+#                         place = destination
+#                         break
+#                     else:
+#                         destination = place
+#                 logging.debug(
+#                     f"FINAL PLACEMENT {element=} {source=} {destination=}")
+#                 source = destination
+#             if destination <= min_destination:
+#                 min_destination = destination
+#             logging.info(f"{min_destination=}")
+# if mode == 'test':
+#     assert min_destination == 46, f"{min_destination=}"
+# if mode == 'production':
+#     assert min_destination == 825516882, f"{min_destination=}"
+with open("puzzle_input.txt") as f:
+    inputs, *blocks = f.read().split("\n\n")
 
+inputs = list(map(int, inputs.split(":")[1].split()))
 
-@dataclasses.dataclass
-class Mapping:
-    destination_range_start: int
-    source_range_start: int
-    range_length: int
+seeds = []
 
-    def __post_init__(self):
-        self.source_range_start = int(self.source_range_start)
-        self.destination_range_start = int(self.destination_range_start)
-        self.range_length = int(self.range_length)
+for i in range(0, len(inputs), 2):
+    seeds.append((inputs[i], inputs[i] + inputs[i + 1]))
 
-    def check_mapping(self, point):
-        return self.source_range_start + self.range_length >= point >= self.source_range_start
+for block in blocks:
+    ranges = []
+    for line in block.splitlines()[1:]:
+        ranges.append(list(map(int, line.split())))
+    new = []
+    while len(seeds) > 0:
+        s, e = seeds.pop()
+        for a, b, c in ranges:
+            os = max(s, b)
+            oe = min(e, b + c)
+            if os < oe:
+                new.append((os - b + a, oe - b + a))
+                if os > s:
+                    seeds.append((s, os))
+                if e > oe:
+                    seeds.append((oe, e))
+                break
+        else:
+            new.append((s, e))
+    seeds = new
 
-    def map_source_to_destination(self, point: int):
-        return self.destination_range_start + (point - self.source_range_start)
-
-
-for element in [
-    "seed-to-soil",
-    "soil-to-fertilizer",
-    "fertilizer-to-water",
-    "water-to-light",
-    "light-to-temperature",
-    "temperature-to-humidity",
-    "humidity-to-location",
-]:
-    index = data.index(f"{element} map:")
-    lines = takewhile(lambda line: line != '', data[index + 1:])
-    cleaned_data[element] = [Mapping(*line.split()) for line in lines]
-    # logging.debug(cleaned_data[element])
-
-logging.debug(cleaned_data)
-
-locations = []
-min_destination = sys.maxsize
-for seed_range in seeds_pairs:
-    places = copy.deepcopy(seed_range)
-    logging.info(f"loop for {places}")
-    for seed in seed_range:
-        logging.debug(f"checking {seed=}")
-        for place in places:
-            source = place
-            for element in [
-                "seed-to-soil",
-                "soil-to-fertilizer",
-                "fertilizer-to-water",
-                "water-to-light",
-                "light-to-temperature",
-                "temperature-to-humidity",
-                "humidity-to-location",
-            ]:
-                destination = None
-                for mapping in cleaned_data[element]:
-                    logging.debug(
-                        f"{element=} source={source}; is_mapped={mapping.check_mapping(place)}; destination={mapping.map_source_to_destination(place)} ")
-                    if mapping.check_mapping(place):
-                        destination = mapping.map_source_to_destination(place)
-                        place = destination
-                        break
-                    else:
-                        destination = place
-                logging.debug(
-                    f"FINAL PLACEMENT {element=} {source=} {destination=}")
-                source = destination
-            if destination <= min_destination:
-                min_destination = destination
-            logging.debug(f"{min_destination=}")
-if mode == 'test':
-    assert min_destination == 46, f"{min_destination=}"
-if mode == 'production':
-    assert min_destination == 825516882, f"{min_destination=}"
+print(min(seeds)[0])
